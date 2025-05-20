3a64106dcdeb240716d6a060ef97e5b522f74dca
Juan6Sam <juansamoficial@gmail.com>
2025-05-20 17:15:42 -0600

    correccion de funcionamiento

---
+++ AndroidStudioProjects/AnnyCase-main/app/src/main/java/com/example/casesanny1/UpdateActivity.java
@@ -0,0 +1,6 @@
+package com.example.casesanny1;
+
+import android.app.Activity;
+
+public class UpdateActivity extends Activity {
+}
---
+++ AndroidStudioProjects/AnnyCase-main/app/src/main/java/com/example/casesanny1/ui/login/UpdateActivity.java
@@ -0,0 +1,16 @@
+package com.example.casesanny1.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.casesanny1.R;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+}
